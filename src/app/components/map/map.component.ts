c8615b0b9b5db8747f140386fe9eff93811fbcb7
StanDarius24 <standarius24@gmail.com>
2021-11-06 20:57:21 +0200

    draw area

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,18 +1,33 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {Loader} from "@googlemaps/js-api-loader";
 import {styles} from "./mapstyles";
+import DirectionsService = google.maps.DirectionsService;
+import DirectionsRenderer = google.maps.DirectionsRenderer;
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnChanges {
 
   title = 'google-maps';
 
-  private map: google.maps.Map
+  @Input()
+  visible: boolean;
+  private map: google.maps.Map;
 
   ngOnInit(): void {
+  }
+
+  initializeMap() {
+
+    let infowindow = new google.maps.InfoWindow();
+    let directionsService = new google.maps.DirectionsService;
+    let directionsDisplay = new google.maps.DirectionsRenderer({
+      suppressPolylines: true,
+      infoWindow: infowindow
+    });
+
     let loader = new Loader({
       apiKey: 'AIzaSyBR6A65P_BM9v4JMyY1ySFlWmwZghK0Fm0'
     })
@@ -28,12 +43,97 @@ export class MapComponent implements OnInit {
         zoom: 6,
         styles: styles
       })
-
+      directionsDisplay.setMap(this.map);
+      this.calculateAndDisplayRoute(directionsService, directionsDisplay, this.map, infowindow);
       const marker = new google.maps.Marker({
         position: location,
         map: this.map,
       });
     })
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['visible']) {
+      this.initializeMap();
+    }
+  }
+
+  calculateAndDisplayRoute(directionsService: DirectionsService, directionsDisplay: DirectionsRenderer, map: google.maps.Map, infowindow: google.maps.InfoWindow) {
+    let waypts = [{
+      location: '41.062317, 28.899756',
+      stopover: true
+    }, {
+      location: '41.062276, 28.898866',
+      stopover: true
+    }, {
+      location: '41.061993, 28.8982',
+      stopover: true
+    }];
+    directionsService.route({
+      origin: {
+        lat: 41.063328,
+        lng: 28.901215
+      },
+      destination: {
+        lat: 41.060756,
+        lng: 28.900046
+      },
+      waypoints: waypts,
+      optimizeWaypoints: true,
+      travelMode: google.maps.TravelMode.DRIVING
+    }, function(response, status) {
+
+      //anonymous
+      function renderDirectionsPolylines(response: google.maps.DirectionsResult) {
+        let polylineOptions = {
+          strokeColor: '#C83939',
+          strokeOpacity: 1,
+          strokeWeight: 4
+        };
+        let colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"];
+        let polylines: Array<any> = [];
+        let bounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < polylines.length; i++) {
+          polylines[i].setMap(null);
+        }
+        let legs = response.routes[0].legs;
+        for (i = 0; i < legs.length; i++) {
+          let steps = legs[i].steps;
+          for (var j = 0; j < steps.length; j++) {
+            let nextSegment = steps[j].path;
+            let stepPolyline = new google.maps.Polyline(polylineOptions);
+            stepPolyline.setOptions({
+              strokeColor: colors[i]
+            })
+            for (var k = 0; k < nextSegment.length; k++) {
+              stepPolyline.getPath().push(nextSegment[k]);
+              bounds.extend(nextSegment[k]);
+            }
+            polylines.push(stepPolyline);
+            stepPolyline.setMap(map);
+            // route click listeners, different one on each step
+            google.maps.event.addListener(stepPolyline, 'click', function(evt: any) {
+              infowindow.setContent("you clicked on the route<br>" + evt.latLng.toUrlValue(6));
+              infowindow.setPosition(evt.latLng);
+              infowindow.open(map);
+            })
+          }
+        }
+        map.fitBounds(bounds);
+      }
+
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setOptions({
+          directions: response,
+        })
+        if(response != null) {
+          let route = response.routes[0];
+          renderDirectionsPolylines(response);
+        }
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
   }
 
 }
